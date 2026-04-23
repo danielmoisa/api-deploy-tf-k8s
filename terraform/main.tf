b62fdf8cffa6694eb946e8d82b26a8c709518d60
Daniel Moisa <danimoisa93@gmail.com>
2026-04-23 18:03:33 +0300

    update tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   region = "us-east-1"
 
-  access_key                  = "anaccesskey"
-  secret_key                  = "asecretkey"
+  access_key                  = "test"
+  secret_key                  = "test"
   s3_use_path_style           = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
@@ -14,25 +14,25 @@ provider "aws" {
   }
 }
 
-data "aws_ami" "linux" {
-  most_recent = true
+# data "aws_ami" "linux" {
+#   most_recent = true
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-2.0.*-x86_64-ebs"]
-  }
+#   filter {
+#     name   = "name"
+#     values = ["amzn2-ami-hvm-2.0.*-x86_64-ebs"]
+#   }
 
-  owners = ["137112412989"] # Amazon
-}
+#   owners = ["137112412989"] # Amazon
+# }
 
-resource "aws_instance" "app_server" {
-  ami           = data.aws_ami.linux.id
-  instance_type = "t2.micro"
+# resource "aws_instance" "app_server" {
+#   ami           = data.aws_ami.linux.id
+#   instance_type = "t2.micro"
 
-  tags = {
-    Name = "hashicorp-learn"
-  }
-}
+#   tags = {
+#     Name = "hashicorp-learn"
+#   }
+# }
 
 resource "aws_s3_bucket" "backend_storage" {
   bucket = "app-data-bucket"
